13862f8954e086fa9163106f9cb63b39e91e5439
bellflower93 <sancirkovic@gmail.com>
2023-07-01 21:10:00 +0200

    Add restart app to GitHub

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Sandra Cirkovic on 1.7.23..
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestartApp/Screens/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  RestartApp
+//
+//  Created by Sandra Cirkovic on 1.7.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
